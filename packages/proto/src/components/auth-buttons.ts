7a181265bbd50ca27d4e68132f7d86041c3ce898
Jake Huey <125854542+jhueyy@users.noreply.github.com>
2025-05-20 00:06:48 -0700

    dark mode fixed and added custom header

---
+++ packages/proto/src/components/auth-buttons.ts
@@ -0,0 +1,44 @@
+// src/components/auth-buttons.ts
+import { html, LitElement } from "lit";
+import { state } from "lit/decorators.js";
+import { Auth, Observer, Events } from "@calpoly/mustang";
+
+export class HeaderButtons extends LitElement {
+  @state()
+  loggedIn = false;
+
+  @state()
+  user?: string;
+
+  private _auth = new Observer<Auth.Model>(this, "blazing:auth");
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._auth.observe((auth) => {
+      const { user } = auth;
+      this.loggedIn = !!(user && user.authenticated);
+      this.user = user?.username;
+    });
+  }
+
+  render() {
+    return html`
+      ${this.loggedIn ? this.renderSignOut() : this.renderSignIn()}
+    `;
+  }
+
+  renderSignIn() {
+    return html`<a href="/login.html">Sign In</a>`;
+  }
+
+  renderSignOut() {
+    return html`
+      <a href="#" @click=${this.signOut}>Sign Out (${this.user})</a>
+    `;
+  }
+
+  signOut(event: Event) {
+    event.preventDefault();
+    Events.relay(event, "auth:message", ["auth/signout"]);
+  }
+}
